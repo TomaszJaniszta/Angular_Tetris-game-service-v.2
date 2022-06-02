fb269a3d8dcb88f2fab1ead369c5719fba2bec27
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-06-02 18:33:10 +0200

    Create filter.pipe.ts

---
+++ Angular_v2/src/app/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { historyList } from './game/game.component';
+
+@Pipe({
+  name: 'filterEvents',
+})
+
+export class FilterPipe implements PipeTransform {
+  transform(items: Array<historyList>,
+            selectedOption: string,
+            historyListLenght?: number // optional to refresh data pipe on change / filter on
+            ): Array<historyList> {
+    if (selectedOption === 'All') {
+      return items
+    } else {
+    let filteredArray = items.filter(
+      (item) => item.gameEvent === selectedOption );
+      return filteredArray;
+    }
+  }
+}
