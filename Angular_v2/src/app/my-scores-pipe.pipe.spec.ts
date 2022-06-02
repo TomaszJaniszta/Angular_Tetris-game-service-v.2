8e9bf6d4bc930e1fb8c6b6ca7ac643902e9b1d40
Tomek <92643157+TomaszJaniszta@users.noreply.github.com>
2022-06-02 18:33:52 +0200

    Create my-scores-pipe.pipe.spec.ts

---
+++ Angular_v2/src/app/my-scores-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MyScoresPipePipe } from './my-scores-pipe.pipe';
+
+describe('MyScoresPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new MyScoresPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
